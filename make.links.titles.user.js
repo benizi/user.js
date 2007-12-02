42fdb3ca20a9704dd34985cbd9aa0cc0cdac415a
bhaskell <devnull@localhost>
2007-12-02 01:45:36 +0000

    [second-half @ 461] Adds 'title' attribute to title-less links with the link's URL.

---
+++ make.links.titles.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name          Make Links Titles
+// @namespace     http://benizi.com/gm
+// @description   Changes all links without "title" attributes to title="(url)"
+// @include       http://*
+// ==/UserScript==
+
+(function () {
+ try {
+  var all = document.evaluate('//a[not(@title)]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+  for (var j = 0; j < all.snapshotLength; j++) {
+   var it = all.snapshotItem(j);
+   var label = it.getAttribute('href');
+   if (!label) label = it.getAttribute('name');
+   if (!label) label = 'no destination?';
+   it.setAttribute('title',label);
+  }
+ } catch (e) { alert("Nope:\n"+e); }
+})();
