8470ec21783af0c28eae55a2f3190159d2a5dc32
Benjamin R. Haskell <benizi@benizi.com>
2010-08-31 00:54:54 -0400

    user.js: jQuery-fied wikipedia.cite

--- wikipedia.cite.user.js
+++ wikipedia.cite.user.js
@@ -3,20 +3,9 @@
 // @namespace     http://benizi.com/gm
 // @description   Changes "citation needed" to "fuck citations!"
 // @include       http://*wikipedia.org/*
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
 
-(function () {
-  var types = new Array(
-   '//sup//a//i//text()',
-   '//sup//span//i//a//text()'
-  );
-  for (var j = 0; j < types.length; j++) {
-   var all = document.evaluate(types[j], document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-   for (var i = 0; i < all.snapshotLength; i++) {
-      var it = all.snapshotItem(i);
-      if (!it.data.match(/citation\s*needed/)) continue;
-      it = it.parentNode;
-      it.replaceChild(document.createTextNode("FUCK CITATIONS!"),it.firstChild);
-   }
-  }
-})();
+$(function(){
+ $('a:contains(citation needed)').text('FUCK CITATIONS!');
+});
