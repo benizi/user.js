a6093d2842cc580aa3498bff3f24e6245313f4c9
Benjamin R. Haskell <benizi@benizi.com>
2011-03-08 19:48:42 -0500

    owa-notifier: use function to allow $ instead of jQuery

--- owa.notifier.user.js
+++ owa.notifier.user.js
@@ -10,33 +10,32 @@
 
 jQuery.noConflict();
 
-function monitorChanges(selector, frequency, callback, onlyonce) {
-   var html = jQuery(selector).html();
-   var listener;
-   listener = setInterval(function(){
-      var newhtml = jQuery(selector).html();
-      if (newhtml == html) return;
-      html = newhtml;
-      callback();
-      if (onlyonce) clearInterval(listener);
-   }, frequency);
-}
-
-(function (){
+(function ($){
+   function monitorChanges(selector, frequency, callback, onlyonce) {
+      var html = $(selector).html();
+      var listener;
+      listener = setInterval(function(){
+         var newhtml = $(selector).html();
+         if (newhtml == html) return;
+         html = newhtml;
+         callback();
+         if (onlyonce) clearInterval(listener);
+      }, frequency);
+   }
    if (!window.webkitNotifications) return;
    if (window.webkitNotifications.checkPermission()) {
-      jQuery('<input type="button"/>')
+      $('<input type="button"/>')
       .value('Setup Notifications')
       .click(function(){
          window.webkitNotifications.requestPermission();
-         jQuery(this).remove();
+         $(this).remove();
       });
    }
    var notification = null;
    monitorChanges('#divAlertBar', 1000, function(){
       if (notification) return;
-      var page = jQuery('<div></div>');
-      var logo = jQuery('#imgLiveLogo');
+      var page = $('<div></div>');
+      var logo = $('#imgLiveLogo');
       var src = logo.attr('src');
       var css = {
          backgroundPosition: logo.css('backgroundPosition'),
@@ -44,20 +43,20 @@ function monitorChanges(selector, frequency, callback, onlyonce) {
          height: logo.css('height'),
          width: logo.css('width')
       };
-      var img = jQuery('<img/>').attr({src:src,border:0}).css(css);
+      var img = $('<img/>').attr({src:src,border:0}).css(css);
       page.append(img);
-      var messages = jQuery('.divNotificationsColumn1');
+      var messages = $('.divNotificationsColumn1');
       if (!messages.length) return;
       messages.each(function(){
-         var div = jQuery('<div></div>').appendTo(page);
-         jQuery('div', this).each(function(){
-            div.append(jQuery('<div></div>').text(jQuery(this).text()));
+         var div = $('<div></div>').appendTo(page);
+         $('div', this).each(function(){
+            div.append($('<div></div>').text($(this).text()));
          });
-         jQuery('div:first', div).css({fontWeight:'bold'});
+         $('div:first', div).css({fontWeight:'bold'});
       });
       var url = 'data:text/html;base64,'+encode_base64('<div>'+page.html()+'</div>');
       notification = window.webkitNotifications.createHTMLNotification(url);
       notification.onclose = function () { notification = null; };
       notification.show();
    });
-})();
+})(jQuery);
